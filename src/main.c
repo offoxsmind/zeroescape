35e36e6f0f2a9c074d8d650339bdf6895fe30caf
offoxsmind <somberfox@gmail.com>
2016-05-11 23:15:31 -0400

    Fixed another memory issue
    I think it's done! . . . hopefully.

--- src/main.c
+++ src/main.c
@@ -7,8 +7,6 @@ static char s_num_buffer[8];
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_map;
 
-#define HOUR_KEY 0
-
 static uint8_t mycolor[24] = { (uint8_t)0b11001100 //ARGB8
                                       ,(uint8_t)0b11011110
                                       ,(uint8_t)0b11110000
@@ -48,8 +46,6 @@ static void update_time() {
   static char s_time_buffer[8];
   strftime(s_time_buffer, sizeof(s_time_buffer), clock_is_24h_style() ?
                                           "%H:%M" : "%I:%M", tick_time);
-
-
   int hour = tick_time->tm_hour;
   
   GColor hour_color = ((GColor)mycolor[hour]);
@@ -105,7 +101,7 @@ static void window_load(Window *s_window) {
   
   
   //Hour
-  s_hour_layer = text_layer_create PBL_IF_ROUND_ELSE((GRect(65, 41, 90, 130)), (GRect(39,49,70,70)));
+  s_hour_layer = text_layer_create PBL_IF_ROUND_ELSE((GRect(63, 40, 90, 130)), (GRect(39,49,70,70)));
   text_layer_set_background_color(s_hour_layer, GColorClear);
   PBL_IF_ROUND_ELSE((text_layer_set_font(s_hour_layer, s_date_font)), (text_layer_set_font(s_hour_layer, s_date_font2)));
   text_layer_set_text_alignment(s_hour_layer, GTextAlignmentCenter);
@@ -113,7 +109,7 @@ static void window_load(Window *s_window) {
   layer_set_hidden((Layer *)s_hour_layer, false);
   
   //Text
-  s_text_layer = text_layer_create(GRect(44, 114, 100, 100));
+  s_text_layer = text_layer_create(GRect(43, 114, 100, 100));
   text_layer_set_background_color(s_text_layer, GColorClear);
   text_layer_set_font(s_text_layer, s_date_font2);
   text_layer_set_text(s_text_layer, "PA IR");
@@ -122,14 +118,12 @@ static void window_load(Window *s_window) {
   layer_set_hidden((Layer *)s_text_layer, false);
 
   //Time
-  s_time_layer = text_layer_create(GRect(20, 55, 140, 70));
+  s_time_layer = text_layer_create(GRect(19, 55, 140, 70));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_font(s_time_layer, s_date_font3);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   layer_add_child(window_get_root_layer(s_window), text_layer_get_layer(s_time_layer));
   layer_set_hidden((Layer *)s_time_layer, true);
-  
-  update_time ();
   
 }
 
@@ -169,7 +163,7 @@ static void deinit() {
 
 
   tick_timer_service_unsubscribe();
-  window_destroy(s_window);
+  accel_tap_service_unsubscribe();
 }
 
 int main() {
